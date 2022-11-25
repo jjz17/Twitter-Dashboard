161b4ab4e125af218934b45a602c9fa68c838bd7
Jason Zhang <jasonjzhang17@gmail.com>
2022-11-25 08:41:39 -0500

    Ignore .env file

--- twitter_dashboard/personal_twitter_data.py
+++ twitter_dashboard/personal_twitter_data.py
@@ -1,2 +1,23 @@
 import tweepy
+from tweepy import OAuthHandler
 from dotenv import load_dotenv
+
+import os
+
+
+# Store keys and tokens for tweepy
+load_dotenv()
+
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_KEY_SECRET = os.getenv("CONSUMER_KEY_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+tweets = []
+
+print(tweepy.Cursor(api.home_timeline).items(10))
